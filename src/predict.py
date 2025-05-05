6f0c101debe324d12fcda62c58497e41994c28d1
Caio Andres <caioandresgit@outlook.com>
2025-05-04 23:39:57 -0300

    feat(predict): Predict emotion function created with parameter that will be passed in main.py file.

---
+++ src/predict.py
@@ -0,0 +1,9 @@
+from keras.api.models import load_model
+from keras_preprocessing.sequence import pad_sequences
+
+def predict_emotion(model, tokenizer, label_encoder, text):
+  seq = tokenizer.texts_to_sequences([text])
+  padded = pad_sequences(seq, maxlen=100, padding="post", truncating="post")
+  prediction = model.predict(padded)
+  class_index = prediction.argmax(axis=1)[0]
+  return label_encoder.inverse_transform([class_index])[0]
